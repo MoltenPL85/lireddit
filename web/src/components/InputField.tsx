d2d5ccac29a4a426e6312a9469179617fee9a360
MoltenPL85 <bugaev.maxim.85@gmail.com>
2020-12-11 19:27:41 +0500

    Added create-post page and global error exchange

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -4,24 +4,32 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
+  InputProps,
 } from '@chakra-ui/react';
 import { useField } from 'formik';
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
+  textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextarea = Input as React.FC<InputProps>;
+  if (textarea) {
+    InputOrTextarea = Textarea as React.FC<InputProps>;
+  }
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <InputOrTextarea {...field} {...props} id={field.name} />
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
   );
