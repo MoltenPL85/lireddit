3fe17f73387d84a60d6d5adb0acfc9aa106f2b42
MoltenPL85 <bugaev.maxim.85@gmail.com>
2020-12-12 20:42:02 +0500

    Posts styling

--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -42,6 +42,7 @@ export type Post = {
   creatorId: Scalars['Float'];
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
+  textSnippet: Scalars['String'];
 };
 
 export type User = {
@@ -240,7 +241,7 @@ export type PostsQuery = (
   { __typename?: 'Query' }
   & { posts: Array<(
     { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'title' | 'updatedAt' | 'createdAt'>
+    & Pick<Post, 'id' | 'title' | 'textSnippet' | 'updatedAt' | 'createdAt'>
   )> }
 );
 
@@ -351,6 +352,7 @@ export const PostsDocument = gql`
   posts(limit: $limit, cursor: $cursor) {
     id
     title
+    textSnippet
     updatedAt
     createdAt
   }
