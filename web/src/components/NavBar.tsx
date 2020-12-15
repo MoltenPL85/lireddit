8af7f260f0f576873c80d33df8a3b62da12587f5
MoltenPL85 <bugaev.maxim.85@gmail.com>
2020-12-15 16:36:21 +0500

    Single post

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, Flex, Link } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
 
@@ -40,7 +40,12 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
   }
 
   return (
-    <Flex zIndex={1} position='sticky' top={0} bg='tan' p={4} ml={'auto'}>
+    <Flex zIndex={1} position='sticky' top={0} bg='tan' p={4} align='center'>
+      <NextLink href='/'>
+        <Link>
+          <Heading>LiReddit</Heading>
+        </Link>
+      </NextLink>
       <Box ml={'auto'}>{body}</Box>
     </Flex>
   );
